b4137721948afb05748d85ae407878f3db27f9c8
Tan Jian Xin <tanjianxin@Admins-iMac.local>
2016-03-02 10:34:03 +0800

    - initial Commit

---
+++ Assets/Flipper.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flipper : MonoBehaviour {
+
+	[SerializeField]float 					swingAngle;
+	[SerializeField]float					normalAngle;
+
+	private	bool							isSwinging;
+	private float							counter = 0;
+
+	float outAngle;
+	float inAngle;
+
+	// Use this for initialization
+	void Start () {
+		isSwinging = false;
+		outAngle = rigidbody2D.rotation + swingAngle;
+		inAngle = rigidbody2D.rotation;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float def = swingAngle - normalAngle;
+		if (isSwinging) {
+			rigidbody2D.MoveRotation (outAngle);
+		} else {
+			rigidbody2D.MoveRotation(inAngle);
+		}
+//		float min = Mathf.Min (rigidbody2D.rotation+swingAngle, rigidbody2D.rotation+normalAngle);
+//		float max = Mathf.Max (rigidbody2D.rotation+swingAngle, rigidbody2D.rotation+normalAngle);
+//		rigidbody2D.rotation = Mathf.Clamp (rigidbody2D.rotation, min, max);
+		//isSwinging = false;
+	
+//		counter += Time.deltaTime;
+//		if (counter >= 0.25f)
+//			isSwinging = false;
+	}
+
+	public void Swing()
+	{
+		isSwinging = true;
+		Debug.Log ("Swing l");
+	}
+
+	public void SwingBack()
+	{
+		isSwinging = false;
+		Debug.Log ("Swing back");
+	}
+}
---
+++ Assets/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+	[SerializeField]Flipper				_flipperLeft;
+	[SerializeField]Flipper				_flipperRight;
+
+	[SerializeField]GameObject			_ballObject;
+
+	[SerializeField]float 				_launchForce;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnLeftFlipperDown()
+	{
+		_flipperLeft.Swing ();
+	}
+
+	public void OnLeftFlipperUp()
+	{
+		_flipperLeft.SwingBack ();
+	}
+
+	public void OnRightFlipperDown()
+	{
+		_flipperRight.Swing ();
+	}
+
+	public void OnRightFlipperUp()
+	{
+		_flipperRight.SwingBack ();
+	}
+
+	public void OnSpringUp()
+	{
+		_ballObject.rigidbody2D.AddForce (new Vector2(0,_launchForce), ForceMode2D.Impulse);
+		Debug.Log ("Launching");
+	}
+}
