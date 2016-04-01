357b1524120f575c2307f8f8f914f38bcfd010f4
Derek lim <derek.lim@gumi.sg>
2016-04-01 15:13:27 +0800

    Updated Home Menu

---
+++ Assets/Scripts/Utility/SceneFadeInOut.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneFadeInOut : MonoBehaviour
+{
+    public float fadeSpeed = 1.5f;          // Speed that the screen fades to and from black.
+    private bool sceneStarting = true;      // Whether or not the scene is still fading in.
+    private bool changeScene = false;
+
+    private string sceneName = "";
+    private int sceneIndex = 0;
+
+//    [SerializeField]UITexture guiTex;
+    
+    
+    void Awake ()
+    {
+//		if(guiTex != null)
+//       	// Set the texture so that it is the the size of the screen and covers it.
+//       	guiTex.transform.localScale = new Vector3(2048, 2048, 1);
+    }
+
+    IEnumerator FadeToClear ()
+    {
+//        // Lerp the colour of the texture between itself and transparent.
+//        while(guiTex.color.a > 0.05f)
+//        {
+//			guiTex.color = Color.Lerp(guiTex.color, Color.clear, fadeSpeed * Time.deltaTime);
+//			yield return null;
+//		}
+//
+//		// ... set the colour to clear and disable the GUITexture.
+//		guiTex.color = Color.clear;
+//		guiTex.enabled = false;
+        
+        // The scene is no longer starting.
+//        sceneStarting = false;
+		yield return null;
+    }
+    
+    
+    IEnumerator FadeToBlack (string name)
+    {
+//    	while(guiTex.color.a < 0.95f)
+//    	{
+//	        // Lerp the colour of the texture between itself and black.
+//			guiTex.color = Color.Lerp(guiTex.color, Color.black, fadeSpeed * Time.deltaTime);
+//			yield return null;
+//		}
+		yield return null;
+		Application.LoadLevel(name);
+    }
+	IEnumerator FadeToBlack (int index)
+    {
+//    	while(guiTex.color.a < 0.95f)
+//    	{
+//	        // Lerp the colour of the texture between itself and black.
+//			guiTex.color = Color.Lerp(guiTex.color, Color.black, fadeSpeed * Time.deltaTime);
+//			yield return null;
+//		}
+		yield return null;
+		Application.LoadLevel(index);
+    }
+    
+    
+    public void StartScene ()
+    {
+        // Fade the texture to clear.
+        StartCoroutine(FadeToClear());
+    }
+    
+    
+    public void ChangeScene (string name)
+    {
+
+        // Make sure the texture is enabled.
+		//guiTex.enabled = true;
+		sceneName = name;
+		// Start fading towards black.
+        StartCoroutine(FadeToBlack(name));
+    }
+
+    public void ChangeScene(int index)
+    {
+		// Make sure the texture is enabled.
+		//guiTex.enabled = true;
+		sceneIndex = index;
+
+		StartCoroutine(FadeToBlack(index));
+    }
+}
